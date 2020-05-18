672ad2b964e4f14a8c4039951dede274febad0a2
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2020-05-18 11:38:26 +0530

    Update 400. To and Fro.py

--- 400. To and Fro.py
+++ 400. To and Fro.py
@@ -27,4 +27,31 @@ while(cols != 0):
     arr[1::2, :] = arr[1::2, ::-1]
     arr = arr.flatten('F')
     listToStr = ''.join([str(elem) for elem in arr])
+    
     print(listToStr)
+
+    
+    
+    
+# Solution 3
+
+while(1):
+    n = int(input())
+    if n==0:
+        break
+    s = input()
+    if n==1:
+        print(s)
+    else:
+        s_list = []
+        s_list = [s[i:i+n][::-1] if ((i/n)%2)!=0 else s[i:i+n] for i in range(0, len(s), n)]
+        s= ""
+        for i in range(0,n):
+            for x in s_list:
+                try:
+                    s = s+ x[i]
+                except:
+                    pass
+        print(s)    
+    
+    

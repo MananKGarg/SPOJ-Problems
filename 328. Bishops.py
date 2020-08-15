4a2ce81b2c7d44d0ffed26a642ebac8f3a8e9bac
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2020-08-15 22:12:15 +0530

    Update 328. Bishops.py

--- 328. Bishops.py
+++ 328. Bishops.py
@@ -1,49 +1,61 @@
 try:
-        for i in range(1024):
-            n = input()
+    n = input()
+    while (int(n) > 0):
 
-            if (int(n) == 1):
-                print(n)
-            elif(int(n) == 5):
-                print("8")
+        if (int(n) == 1):
+            print(n)
+        elif (int(n) == 5):
+            print("8")
 
-            else:
+        else:
 
-                n_str = list(n)
-                n_str.reverse()
+            n_str = list(n)
+            n_str.reverse()
 
-                carry = 0
+            carry = 0
 
-                for i in range(len(n_str)):
+            for i in range(len(n_str)):
 
-                    if (int(n_str[i]) < 5):
-                        n_str[i] = str(2*(int(n_str[i])) + carry)
-                        carry = 0
-
-                    else:
-                        n_str[i] = str(((2*(int(n_str[i]))) - 10) + carry)
-                        carry = 1
-
-                if (carry == 1):
-                    n_str.append("1")
-
-                if(int(n_str[0]) > 0):
-
-                    n_str[0] = str(int(n_str[0]) - 2)
+                if (int(n_str[i]) < 5):
+                    n_str[i] = str(2 * (int(n_str[i])) + carry)
+                    carry = 0
 
                 else:
-                    n_str[0] = "8"
-                    l = 1
-                    while( int(n_str[l]) == 0 ):
-                        n_str[l] = "9"
-                        l = l + 1
-                    n_str[l] = str(int(n_str[l]) - 1)
+                    n_str[i] = str(((2 * (int(n_str[i]))) - 10) + carry)
+                    carry = 1
 
-                n_str.reverse()
+            if (carry == 1):
+                n_str.append("1")
 
-                n_str = "".join(n_str)
-                print(n_str)
+            if (int(n_str[0]) > 0):
 
+                n_str[0] = str(int(n_str[0]) - 2)
+
+            else:
+                n_str[0] = "8"
+                l = 1
+                while (int(n_str[l]) == 0):
+                    n_str[l] = "9"
+                    l = l + 1
+                n_str[l] = str(int(n_str[l]) - 1)
+
+            n_str.reverse()
+
+            n_str = "".join(n_str)
+            print(n_str)
+
+        n = input()
 except:
     pass
+
+
+
+
+
+
+
+
+
+
+
 

bce4b982b7f1968ee4733d9a9e29e6e87ac514e3
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2020-08-15 22:16:33 +0530

    Update 328. Bishops.py

--- 328. Bishops.py
+++ 328. Bishops.py
@@ -9,7 +9,7 @@ try:
 
         else:
 
-            n_str = list(n)
+            n_str = list(n)        # convert string to list of characters (digits)
             n_str.reverse()
 
             carry = 0
@@ -17,11 +17,11 @@ try:
             for i in range(len(n_str)):
 
                 if (int(n_str[i]) < 5):
-                    n_str[i] = str(2 * (int(n_str[i])) + carry)
+                    n_str[i] = str(2 * (int(n_str[i])) + carry)      # if n_str[i] == 4, then 2n have 8 in the corresponding position
                     carry = 0
 
                 else:
-                    n_str[i] = str(((2 * (int(n_str[i]))) - 10) + carry)
+                    n_str[i] = str(((2 * (int(n_str[i]))) - 10) + carry)   # if n_str[i] == 6, then 2n, will be 12, but only two would remain and one would go as carry
                     carry = 1
 
             if (carry == 1):
@@ -29,10 +29,10 @@ try:
 
             if (int(n_str[0]) > 0):
 
-                n_str[0] = str(int(n_str[0]) - 2)
+                n_str[0] = str(int(n_str[0]) - 2)   # if the last digit is greater then 0
 
             else:
-                n_str[0] = "8"
+                n_str[0] = "8"                      # if last digit is equal to 0
                 l = 1
                 while (int(n_str[l]) == 0):
                     n_str[l] = "9"
@@ -41,7 +41,7 @@ try:
 
             n_str.reverse()
 
-            n_str = "".join(n_str)
+            n_str = "".join(n_str)                  # make a string again
             print(n_str)
 
         n = input()
